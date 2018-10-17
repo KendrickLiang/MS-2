668b5f946f93e5964847ed8fbe272a2308177e83
StefanTan18 <stan3@stuy.edu>
2018-10-17 12:34:32 -0400

    added db.py

---
+++ v0/db.py
@@ -0,0 +1,23 @@
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitates CSV I/O
+
+DB_FILE="weblog.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+command = """
+CREATE TABLE users(
+user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT,
+password TEXT,
+blog_id INTEGER)
+"""
+c.execute(command)
+
+command2 = "INSERT INTO users VALUES(0, 'a', 'b', 2)"
+
+c.execute(command2)
+
+db.commit()
+db.close()
