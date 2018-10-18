5f87f47b546cff11a1628a3bb8b36fa295d540c7
Michelle Tang <mtang1@stuy.edu>
2018-10-18 12:10:24 -0400

    ?

--- v0/db.py
+++ v0/db.py
@@ -6,17 +6,10 @@ DB_FILE="weblog.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
-command = """
-CREATE TABLE users(
-user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-username TEXT,
-password TEXT,
-blog_id INTEGER)
-"""
+command = "CREATE TABLE {}({1},{2},{3})".format\
+("users", "user_id INTEGER PRIMARY KEY AUTOINCREMENT", "username TEXT", "password TEXT", "blog_id INTEGER")
 c.execute(command)
-
 command2 = "INSERT INTO users VALUES(0, 'a', 'b', 2)"
-
 c.execute(command2)
 
 db.commit()
